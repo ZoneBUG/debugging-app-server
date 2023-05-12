5c8236b6693c246757542e7b8c1fc6def9503f32
suhhyun <97878992+suhhyun524@users.noreply.github.com>
2023-05-12 16:15:22 +0900

    [feat] repository & specification 작성 #31

--- src/main/java/com/zonebug/debugging/domain/post/PostRepository.java
+++ src/main/java/com/zonebug/debugging/domain/post/PostRepository.java
@@ -12,7 +12,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     Page<Post> findAll(Specification<Post> spec, Pageable pageable);
     Optional<Post> findByIdAndUserId(Long id, Long userId);
-
     Optional<Post> findById(Long id);
 
     void deleteById(Long id);
